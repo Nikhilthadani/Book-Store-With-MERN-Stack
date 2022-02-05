302ed09085e8f20eae9530bf234f9d218440c65c
Nikhil Thadani <nikhilthadani112256@gmail.com>
2022-02-05 13:05:46 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import AddBook from "./components/AddBook";
+import Books from "./components/Book/Books";
+import About from "./components/About";
+import BookDetail from "./components/Book/BookDetail";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header>
+        <Header />
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/add" element={<AddBook />} exact />
+          <Route path="/books" element={<Books />} exact />
+          <Route path="/about" element={<About />} exact />
+          <Route path="/books/:id" element={<BookDetail />} exact />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
